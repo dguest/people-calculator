887ecdc00382eed5b73e2a000469884c2f22fc49
Dan Guest <dguest@cern.ch>
2019-10-06 15:02:31 +0200

    add people

---
+++ demi.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+people = {
+    'Danny',
+    'Matthew',
+    'Serhan',
+    'Tulin',
+    'Dan',
+    'Claire',
+    'Grace',
+    'Brendan',
+    'Bing',
+    'Sam',
+}
+
+print(f'we have {len(people)} people')
+
